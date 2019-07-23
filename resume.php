c086e49365ea777a3a6e86e97f6126468f7ed8b6
Asmasabir0306 <52177246+Asmasabir0306@users.noreply.github.com>
2019-07-23 22:38:16 +0500

    Add files via upload
    cv html and css

---
+++ resume.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Resume</title>
+        <!--THIS IS FOR FONT AWESOME-->
+<link href="./fonts/css/font-awesome.min.css" rel="stylesheet">
+
+
+        <!-----css---->
+            <style>
+            img{
+               padding:2%;
+
+            }
+            *{
+
+                font:100% arial,Helvetica,sans-sarif;
+                font-weight:normal;
+               
+                
+            }
+           #resume{
+                font:100% arial,Helvetica,sans-sarif;
+                font-weight:normal;
+                text-align:justify;
+                font-size:20Px;
+                text-align:center;
+                color:black;
+
+
+            }
+            #b{
+                
+                font:100% arial,Helvetica,sans-sarif;
+                font-weight:normal;
+               
+               padding-top:50px;
+                font-size:20Px;
+                text-align:center;
+                color:black;
+
+            }
+
+
+
+            </style>
+    </head>
+    <body>
+    <img alt = "image" src="./image/leftmenu_top.png"width="100%"height="50px"style=border-radius:50px;border:2px solid blue; />
+        <center>
+        <table cellspacing ="0" cellpadding ="2" border="0">
+       
+            
+            <tr>
+                
+                <td colspan="4"id=resume><center><strong><h1>Asma Eman<h1></strong></center></td>
+                <td><img alt = "image" src="./image/resume.jpg"width="300px"height="50%" style=border-radius:20%;/></td>
+                
+            </tr>
+           
+
+            <tr>
+                    <td rowspan="22" style=" background-color:#c5c7d5;" id=b> <b>  <h2>About:</h2>
+                    </b><br><p id=a>My name is Asma Eman. I am passionate and energetic<br> with strong organizational,
+                         creative and communication skills<br> along with the ability to prioritize tasks and exceed<br>
+                          expectations while understrict deadlines.</p><br><br><br><br>Email :<br>asmasabir0306@gmail.com<br><br>Address :<br>
+                          Shamsher Town Sargodha  House No.500 Sargodha<br><br><br><br><br>Contact No# :<br>0303-1418714</h3></td>
+                        <td colspan="4"><h4>// Personal Bio Data<h4><hr></td>
+                        
+             </tr>
+             <tr>
+                        
+                        <td colspan="2"><b>Father’s Name :<b></td>
+                        <td colspan="2">Muhammad Sabir </td>
+                     </tr>
+                     <tr>
+                      
+                        <td colspan="2"><b>Date of Birth :</b> </td>
+                        <td colspan="2">04-01-1995</td>
+                     </tr>
+                     <tr>
+                        <td colspan="2"><b>CNIC# :</b></td>
+                        <td colspan="2">38403-9233218-8</td>
+                     </tr>
+                     <tr>
+                        <td colspan="2"><b>Religion :</b> </td>
+                        <td colspan="2">Islam</td>
+                     </tr>
+                     <tr>
+                        <td colspan="2"><b> Nationality :</b> </td>
+                        <td colspan="2"> Pakistani</td>
+                     </tr>
+                     <tr>
+                        <td colspan="2"><b>Marital Status:</b> </td>
+                        <td colspan="2">Single</td>
+                     </tr>
+                     <tr>
+                        <td colspan="4"><h4>//Qualification</h4><hr></td>
+                    </tr>
+                     <tr>
+                                   
+                        <td colspan="2"><h5>Degree<h5></td>
+                         <td><h5>Board<h5></td>
+                         <td><h5>Percentage<h5></td>
+                    </tr>
+                     <tr>
+                         <td colspan="2"><b>Msc(IT)</b></td>
+                        <td>University Of Sargodha</td>
+                        <td>3.35CGPA</td>
+                     </tr>
+                    <tr>
+                        <td colspan="2"><b>B.Sc</b></td>
+                        <td>University Of Sargodha</td>
+                         <td>65%</td>
+                    </tr> 
+                    <tr>
+                        <td colspan="2"><b>FA(IT)</b></td>
+                        <td> Sargodha</td>
+                         <td>67%</td>
+                    </tr>  
+                    <tr>
+                        <td colspan="2"><b>Matric</b></td>
+                        <td> Sargodha Board</td>
+                         <td>72%</td>
+                    </tr> 
+                    <tr>
+                        <td colspan="4"><h4>//Diploma & Other Certificates<h4><hr></td>
+                       
+                         
+                    </tr>  
+                    <tr>
+                        <td colspan="2"><b> E-Commerce</b></td>
+                        <td colspan="2"> Logix College Sargodha</td>
+                         
+                    </tr>    
+                    <tr>
+                        <td colspan="2"><b>Microsoft Office</b></td>
+                        <td colspan="2">  Vocation Training Institude Sargodha</td>
+                         
+                    </tr>   
+                    <tr>
+                        <td colspan="4"><h4>//Skills</h4><hr></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><h5>Professional Skills</h5></td>
+                        <td colspan="2"><h5>Personal Skills</h5></td>
+                       
+                    </tr>
+                    <tr>
+                        <td> Ms Office</td>
+                        <td>99%</td>
+                        <td> Confident</td>
+                        <td>90%</td>
+                    </tr>  
+                    <tr>
+                        <td> Windows</td>
+                        <td>98%</td>
+                        <td>Commitment</td>
+                        <td>100%</td>
+                    </tr>  
+                    <tr>
+                        <td>  Web Skills</td>
+                        <td>90%</td>
+                        <td>Management</td>
+                        <td>98%</td>
+                    </tr> 
+                    <tr>
+                        <td> Programming</td>
+                        <td>87%</td>
+                        <td>Hard Working</td>
+                        <td>100%</td>
+                    </tr>
+        
+        </table>
+        <img alt = "image" src="./image/leftmenu_bottom.png"width="100%"height="50px"style=border-radius:10%; />
+        </center>
+    </body>
+</html>
