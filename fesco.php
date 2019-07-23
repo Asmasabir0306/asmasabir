9cd0898bdec19c36ca1aa9ab8d92174e8909ba03
Asmasabir0306 <52177246+Asmasabir0306@users.noreply.github.com>
2019-07-23 22:39:05 +0500

    Add files via upload
    bill in html and css

---
+++ fesco.php
@@ -0,0 +1,231 @@
+
+<!DOCTYPE html>
+<html>
+
+
+    <head>
+        <title> www. fesco.com.pk</title>
+        <!-----css---->
+        <link rel="stylesheet" type="text/css" href="./style.css">
+		<style>
+		*{
+        
+                margin:o auto;
+                font-family:Time new Roman:serif;
+                font-weight:normal;
+                text-align:justify;
+                background-color:#1ea06a;
+                background-color:#210f0f03;
+                
+                margin:10px;
+                padding:5px;
+               
+                
+
+                
+
+            }
+               body{
+		        background:linear-gradient(white,#6666);
+		        border:2px solid black;
+		        background-image:url('./image/image 33.jpg');
+		        background-attachment:fixed;
+		        position:relative;
+		        
+                    margin:0 auto;
+                  
+        }
+        #c{
+            font-size: :2px;
+        }
+        #a{
+            font-color:rgb(46, 13, 13);
+            font-color:sky blue;
+        }
+
+        
+		
+		
+		</style>
+            
+    </head>
+    <body>
+       
+          
+        	
+        <table cellspacing ="0" cellpadding ="15" border="0" width=100%>
+            <tr id=c width=100%>
+                     <th><img src="image/fesco.jpg" height ="40px" weigth="80px"/>
+                     <th> <font size="8" color="blue">FAISLABAD ELECTRIC SUPPLY COMPANY</font><br><font size="8" color=" sky blue">ELECTRICITY CONSUMER BILL</font>
+                     <br><font size="8" color="sky  blue">www.fesco.com.pk</font></th>
+                     <th><font size="4" color="blue">FESCO GST NO.<br>24-00-2716-001-19<br> fesco ntn#304866-0</font></th>
+                    
+            </tr>
+            </table>
+           
+             <table cellspacing ="0" cellpadding ="33" border="1" width=100%>
+                 
+                 <tr id=a>
+                     <td>CONNECTION DATE</td>
+                     <td>CONNECTED LOAD </td>
+                     <td>ED@</td>
+                     <td>BILL MONTH</td>
+                     <td> READING DATE</td>
+                     <td>ISSUE DATE</td>
+                     <td>DUE DATE   </td>
+                 </tr>
+                 <tr>
+                     <td>09  MAR 04</td>
+                     <td></td>
+                     <td>1.5%</td>
+                     <td>May 19</td>
+                     <td>30 May 19</td>
+                     <td>31 May 19</td>
+                     <td>11 JUN 19</td>
+                 </tr>
+                </table>
+
+                <table cellspacing ="0" cellpadding ="30" border="1" width=100%>
+            
+                 <tr id=a>
+                     <td><font color="sky blue">CONSUMER ID</font> </td>
+                     <td><font color="sky blue">TARIFF</font> </td>
+                     <td><font color="sky blue">LOAD</font> </td>
+                     <td colspan="2"><font color="sky blue">OLD A/C NUMBER</font> </td>
+                     <td colspan="2"><font color="sky blue">DIVISION</font> </td>
+                     <td colspan="2"><font color="sky blue"> SARGODHA</font> </td>
+                     
+                     
+                 </tr>
+                 <tr>
+                     <td>13789898364394</td>
+                     <td>A-1A(01)</td>
+                     <td>1.50</td>
+                     <td colspan="2">56878909096534</td>
+                     <td colspan="2">SUB DIVISION</td>
+                     <td colspan="2">SATELLITE TOWN</td>
+                     
+                 </tr>
+                 <tr id=a>
+                    <td colspan="2"><font color="sky blue">REFERENCE NO</font></td>
+                    <td> <font color="sky blue">LOCK AGE</font></td>
+                    <td> <font color="sky blue">NO OF ACs</font></td>
+                    <td><font color="sky blue">Unbill. Age</font></td>
+                    <td colspan="2"><font color="sky blue">FEEDER NAME</font></td>
+                    <td colspan="2"><font color="sky blue">CANAL PARK</font></td>
+                 </tr>
+                 <tr>
+                    <td colspan="2">19 13423  237447-U</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>MONTH</td>
+                    <td>UNITS</td>
+                    <td>BILL</td>
+                    <td>PAYMENT</td>
+                 </tr>
+                 <tr>
+                    <td colspan="5"> <font size="8"color="sky blue">NAME & ADDRESS<br>SABIR HUSSAIN<br><br>SATELLITE TOWN<br>SARGODHA</font></td>
+                    <td> MAY 18<br>JUN<br>JUL<br>AUS<br>SEP<br>OCT<br>NOV<br>DEC<br>JAN 19<br> FEB<br>MAE<br>APR</td>
+                    <td>256<br>875<br>567<br>757<br>100<br>109<br>344<br>900<br>176<br>755<br>467<br>234</td>
+                    <td>5256<br>8575<br>5637<br>7527<br>1060<br>1209<br>3454<br>9200<br>1876<br>7155<br>4267<br>2134</td>
+                    <td>5256<br>8575<br>5637<br>7527<br>1060<br>1209<br>3454<br>9200<br>1876<br>7155<br>4267<br>2134</td>
+                 </tr>
+                 <tr>
+                    <td>METER NO</td>
+                    <td>PREVIOUS</td>
+                     <td>PRESENT</td>
+                   
+                    <td>UNITS</td>
+                    <td>STATUS</td>
+                 </tr>
+                 <tr>
+                    <td>S-P 500122</td>
+                    <td>28402</td>
+                     <td>28619</td>
+                    
+                    <td>278</td>
+                    <td></td>
+                 </tr>
+                 </table>
+                 <table cellspacing ="0" cellpadding ="18" border="1" width=100%>
+                    <tr>
+                            <td colspan="2"><font size="7"color="sky blue">FESCO CHARGES</font></td>
+                            <td colspan="2"><font size="7"color="sky blue">GOVT CHARHES</font></td>
+                            <td colspan="2"><font size="7"color="sky blue">TOTAL CHARGES</font></td>
+
+                    </tr>
+                    <tr>
+                            <td><font color="sky blue">UNIT  CONSUMED</font></td>
+                            <td>217</td>
+                            <td><font color="sky blue">ELECTRICTY DUTY</font></td>
+                            <td>56.98</td>
+                            <td><font color="sky blue">ARREAR/AGE</font></td>
+                            <td></td>
+
+                    </tr>
+                    <tr>
+                            <td><font color="sky blue">COST OF  ELECTRICITY</font></td>
+                            <td>1890</td>
+                            <td><font color="sky blue">TV FEE</font></td>
+                            <td>35</td>
+                            <td><font color="sky blue">CURRENT BILL</font></td>
+                            <td>2296</td>
+
+                    </tr>
+                    <tr id=a>
+                            <td><font color="sky blue">METER RENT</font></td>
+                            <td></td>
+                            <td><font color="sky blue">GST</font></td>
+                            <td></td>
+                            <td><font color="sky blue"></font>BILL ADJUSTMENT</td>
+                            <td></td>
+
+                    </tr>
+                    <tr id=a>
+                            <td><font color="sky blue">SERVICE RENT </font></td>
+                            <td></td>
+                            <td><font color="sky blue">INCOME TAX </font></td>
+                            <td></td>
+                            <td><font color="sky blue">INSTSLLMENT</font></td>
+                            <td></td>
+
+                    </tr>
+                    <tr id=a>
+                            <td><font color="sky blue">FULL PRICE ADJUSTMENT</font></td>
+                            <td></td>
+                            <td><font color="sky blue">EXTRA TAX</font></td>
+                            <td></td>
+                            <td><font color="sky blue">SUBSIDIES</font></td>
+                            <td></td>
+
+                    </tr>
+                    <tr id=a>
+                            <td><font color="sky blue">F.C SURCHARGE</font></td>
+                            <td>95.89</td>
+                            <td><font color="sky blue">FURTHER TAX</font></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+
+                    </tr>
+                    <tr id=a>
+                            <td><font color="sky blue">T.R SURCHARGES</font></td>
+                            <td></td>
+                            <td><font color="sky blue">N.J SURCHARGE</font></td>
+                            <td>21.70</td>
+                            <td><font color="sky blue">PAYABLE WITHIN DUE DATE </font></td>
+                            <td>7357</td>
+
+                    </tr>
+                   
+
+
+
+             </table>
+             </table>
+
+
+        </table>
+    </body>
+</html>
